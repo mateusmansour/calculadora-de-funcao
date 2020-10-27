994087e8f35b38bfabbf48d56f0c8978a2c5c1a8
mateusmansour <64480742+mateusmansour@users.noreply.github.com>
2020-10-27 20:00:11 -0300

    removeLocationHash

--- calc.js
+++ calc.js
@@ -1,3 +1,19 @@
+function removeLocationHash() {
+    var noHashURL = window.location.href.replace(/#.*$/, '');
+    window.history.replaceState('', document.title, noHashURL)
+}
+window.addEventListener("popstate", function (event) {
+    removeLocationHash();
+});
+window.addEventListener("hashchange", function (event) {
+    event.preventDefault();
+    removeLocationHash();
+});
+window.addEventListener("load", function () {
+    removeLocationHash();
+});
+
+
 var element = document.getElementById('resultado1');
 
 
@@ -5,37 +21,37 @@ var element = document.getElementById('resultado1');
 var value;
 let params = new URLSearchParams(document.location.search.substring(1));
 
-var mode = params.get("select"); 
+var mode = params.get("select");
 var value = params.get("value");
 var x = value;
 
 function f(x) {
-    result = 3 * x - 7;      
-    return result; 
+    result = 3 * x - 7;
+    return result;
 }
 function g(x) {
     result = 7 * x - 9;
-    return result; 
+    return result;
 }
-if(mode == "f") {
+if (mode == "f") {
     f(x);
-    element.appendChild(document.createTextNode(' '+ result));
-    
+    element.appendChild(document.createTextNode(' ' + result));
+
 }
-else if(mode == "g") {
+else if (mode == "g") {
     g(x);
-    element.appendChild(document.createTextNode(' '+ result));
-    
+    element.appendChild(document.createTextNode(' ' + result));
+
 }
-else if(mode == "fog") {
+else if (mode == "fog") {
     f(g(x));
-    element.appendChild(document.createTextNode(' '+ result));
-    
+    element.appendChild(document.createTextNode(' ' + result));
+
 }
-else if(mode == "gof") {
+else if (mode == "gof") {
     g(f(x));
-    element.appendChild(document.createTextNode(' '+ result));
-    
+    element.appendChild(document.createTextNode(' ' + result));
+
 }
 else {
     //do nothing
